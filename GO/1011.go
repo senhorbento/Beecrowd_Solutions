671c095fc42cc26f596c4103ce3143e1fbd58d01
Henrique Bento <68090144+senhorbento@users.noreply.github.com>
2022-02-12 09:02:48 -0300

    Update 1011.go

--- GO/1011.go
+++ GO/1011.go
@@ -3,9 +3,9 @@ package main
 import "fmt"
 
 func main() {
-    var a int
-    fmt.Scanln(&a)
-    e := float64(a) 
-    r := float64((4/3.0)*3.14159*(e*e*e))
+    	var a int
+    	fmt.Scanln(&a)
+    	e := float64(a) 
+    	r := float64((4/3.0)*3.14159*(e*e*e))
 	fmt.Printf("VOLUME = %.3f\n", r)
 }
