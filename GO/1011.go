4de334bd24f6101210506b50a7108f3675bc14ed
Henrique Bento <68090144+senhorbento@users.noreply.github.com>
2022-02-12 09:02:25 -0300

    Create 1011.go

---
+++ GO/1011.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+    var a int
+    fmt.Scanln(&a)
+    e := float64(a) 
+    r := float64((4/3.0)*3.14159*(e*e*e))
+	fmt.Printf("VOLUME = %.3f\n", r)
+}
