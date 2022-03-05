5fce20c4c858574562427ee8999d9d2644f86bef
Henrique Bento <68090144+senhorbento@users.noreply.github.com>
2022-03-05 14:11:24 -0300

    Create 1924.java

---
+++ Java/1924.java
@@ -0,0 +1,11 @@
+import java.io.IOException;
+
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+ 
+        System.out.println("Ciencia da Computacao");
+ 
+    }
+ 
+}
