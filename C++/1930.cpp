1394f54ac6e141377315a7fa3b27249ffc9fd221
Henrique Bento <68090144+senhorbento@users.noreply.github.com>
2022-03-07 23:11:59 -0300

    Create 1930.cpp

---
+++ C++/1930.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main() {
+ 
+    int t1,t2,t3,t4;
+    
+    cin >> t1 >> t2 >> t3 >> t4;
+    cout << ((t1-1)+(t2-1)+(t3-1)+t4) << endl;
+ 
+    return 0;
+}
