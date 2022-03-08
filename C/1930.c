31d94b40573bc01e9ba2edb73b6e127ed2b48b8d
Henrique Bento <68090144+senhorbento@users.noreply.github.com>
2022-03-07 23:10:26 -0300

    Create 1930.c

---
+++ C/1930.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+    int t1,t2,t3,t4;
+    
+    scanf("%d %d %d %d", &t1, &t2, &t3, &t4);
+    printf("%d\n",((t1-1)+(t2-1)+(t3-1)+t4));
+    
+ 
+    return 0;
+}
